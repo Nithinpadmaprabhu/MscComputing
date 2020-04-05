e512a0eb192831dbb7d479fd030c0d8bdd65d6b7
npadmaprabhu <npadmaprabhu@tssg.org>
2020-04-05 13:17:42 +0100

    first commit

---
+++ 02_objects.js
@@ -0,0 +1,16 @@
+const me = { 
+	name : "Diarmuid O'Connor", // enclose string in " " when it contains a '.   
+	address : '1 Main Street',
+	age : 21,                
+	bankBalance : 20.2,   // millions!!
+	male : true    // no comma for the last property
+} ;
+
+console.log (me.name + ' lives at ' + me['address']) ;
+// Can also use a variable in subscript notation
+const bb = 'bankBalance' ;
+// me.key won't work
+console.log('Balance = ' + me[bb]) ;
+// Changing a property value
+me.address = '2 Main Street' ;
+console.log (me.name + ' now lives at ' + me['address']) ;
---
+++ 05_arrays.js
@@ -0,0 +1,34 @@
+// Array declaration and referencing
+let nums = [12, 22, 5, 28];
+// console.log(nums[2]) ; //  5
+
+// ES5
+// for (let i = 0; i < nums.length; i++) {
+//    console.log(nums[i] )
+// }
+
+// ES6
+// for (const n of nums) {
+//   console.log(n);
+// }
+
+// Manipulating arrays.
+// push/pop - add/remove at the tail
+// nums.push(10); // add to tail of array
+// console.log("After push:\t\t " + nums);
+
+// let element = nums.pop(); // remove from tail of array
+// console.log("After pop:\t\t" + nums);
+// console.log("Removed: " + element);
+
+// shift/unshift - remove/add at the head
+// nums.unshift(3);
+// console.log("After unsfift:\t" + nums);
+
+// element = nums.shift();
+// console.log("After shift:\t\t" + nums);
+// console.log("Removed: " + element);
+
+// nums.forEach(function(n, index, ary) {
+//   console.log(`${n} at index ${index} of ${ary}`);
+// });
