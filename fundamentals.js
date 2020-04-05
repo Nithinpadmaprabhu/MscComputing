5b4920e21657710a3affe4f86a62abcf0b6eabe7
npadmaprabhu <npadmaprabhu@tssg.org>
2020-04-05 14:13:46 +0100

    Arrays

--- fundamentals.js
+++ fundamentals.js
@@ -7,9 +7,36 @@ const aCar = {
     },
     registration : {
         year : '2020', county : 'CA',number : '1717',
-    }
-    
+    },
+    milage : '100',
+    color : {   
+        exterior: "red",
+        interior: { texture: "leather", shade: "cream" }
+    },
+    previous_owners: [
+        "Pat Smith - 1 Main Street",
+        "Sheila Dwyer - 2 High Street"
+      ],
 };
 
-console.log(aCar.registration.county);
+
+console.log('First owner : ' + aCar.previous_owners[0] )
 console.log(aCar.owner + ' drives a ' + aCar.type.make);
+console.log(
+    "Reg. = " +
+      aCar.registration.year +
+      "-" +
+      aCar.registration.county +
+      "-" +
+      aCar.registration.number
+  );
+
+  console.log(
+    "It is a " +
+      aCar.color.exterior +
+      " car, " +
+      aCar.milage +
+      " milage, with " +
+      aCar.color.interior.texture +
+      " interior."
+  );
